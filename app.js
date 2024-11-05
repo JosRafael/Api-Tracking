99bdf214942539af80b3c8be6704c5ea0e56d9d5
JosRafael <rafaelmatiashue@gmail.com>
2024-11-05 18:56:42 -0300

    ajuste cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const pixelController = require('./controllers/pixelController'); 
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
